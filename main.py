5a65309af6d55e12e636dc97e9d25b17c06789b8
Alemoum <marcosvmm3110@gmail.com>
2025-04-18 01:08:24 -0300

    adding the get_count_char function

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #importing the function to get the num of words
 from stats import get_num_words
+from stats import get_count_char
 
 #function to print the book content
 def main():
-    get_num_words() #calling the function
     
+    get_num_words() #calling the function to count words in the text
+    
+    get_count_char() #calling the function to count characters in the text
 
 #execute program
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,6 @@
 #books
 frankenstein = "/home/alemoum/workspace/github.com/Alemoum/bookbot/books/frankenstein.txt"
+char_dict = {}
 
 #funtion to get the book content
 def get_book_text(file_path):
@@ -13,3 +14,14 @@ def get_num_words():
     splited_book_text = book_text.split()
     num_words = len(splited_book_text)
     print(f"{num_words} words found in the document")
+    
+#function to counter characters
+def get_count_char():
+    book_text = get_book_text(frankenstein)
+    book_lower =  book_text.lower()
+    for c in book_lower:
+        if c in char_dict:
+            char_dict[c] +=1
+        else:
+            char_dict.update({c:1})
+    print(char_dict)
